c0b771e9923c17ae733b2acec05ed42212604183
liufu.lf <liufu.lf@antfin.com>
2021-02-27 17:13:36 +0800

    fix: push 地址动态设置

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
  # @version: 0.0.1
  # @Author: fujin
  # @Date: 2020-09-11 15:49:29
- # @LastEditTime: 2021-02-26 16:30:21
+ # @LastEditTime: 2021-02-27 17:13:05
 ### 
 export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin::/usr/local/bin
 # set -e;
@@ -14,31 +14,31 @@ echo $PATH
 
 cd ../G2Plot
 
-npm i
+# npm i
 
 echo "\033[49;32m \n******* G2Plot installing *******\n \033[0m"
 
-npm run dist
+# npm run dist
 
 echo "\033[49;32m \n******* client building *******\n \033[0m"
 
 cd ../release-inspection-of-antv/client
 
-npm i
+# npm i
 
 echo "\033[49;32m \n******* client installing *******\n \033[0m"
 
-npm run build
+# npm run build
 
 echo "\033[49;32m \n******* client building *******\n \033[0m"
 
 cd ../server
 
-npm i
+# npm i
 
 echo "\033[49;32m \n******* server installing *******\n \033[0m"
 
-npm start
+# npm start
 
 # echo "\033[49;32m \n******* server starting *******\n \033[0m"
 
