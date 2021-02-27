eb4ad624b719e9e586468910e748a8ace33a13a6
liufu.lf <liufu.lf@antfin.com>
2021-02-27 18:48:32 +0800

    disable cache

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-###
- # @version: 0.0.1
- # @Author: fujin
- # @Date: 2020-09-11 15:49:29
- # @LastEditTime: 2021-02-26 16:30:21
-### 
 export PATH=$PATH:/bin:/sbin:/usr/bin:/usr/sbin::/usr/local/bin
 # set -e;
 
@@ -12,46 +6,38 @@ echo $PATH
 
 # project_name=release-inspection-of-antv
 
-cd ../G2Plot
+# cd ../G2Plot
 
-npm i
+# echo "\033[49;32m \n******* G2Plot installing *******\n \033[0m"
 
-echo "\033[49;32m \n******* G2Plot installing *******\n \033[0m"
+# npm i
 
-npm run dist
+# echo "\033[49;32m \n******* client building *******\n \033[0m"
 
-echo "\033[49;32m \n******* client building *******\n \033[0m"
+# npm run dist
 
-cd ../release-inspection-of-antv/client
-
-npm i
+cd ./release-inspection-of-antv/client
 
 echo "\033[49;32m \n******* client installing *******\n \033[0m"
 
-npm run build
+npm i
 
 echo "\033[49;32m \n******* client building *******\n \033[0m"
 
-cd ../server
+npm run build
 
-npm i
+cd ../server
 
 echo "\033[49;32m \n******* server installing *******\n \033[0m"
 
-npm start
+npm i
 
-# echo "\033[49;32m \n******* server starting *******\n \033[0m"
+echo "\033[49;32m \n******* server starting *******\n \033[0m"
+
+npm start
 
 # cd ..
 
 # time=$(date "+%Y-%m-%d-%H-%M-%S")
 
 # git checkout -b "${time}"
-
-# git add .
-
-# git commit -m "build"
-
-# git push --set-upstream origin "${time}"
-
-# echo "\033[49;32m \n******* 比对结束，请到 github 上查看，分支：${time} *******\n \033[0m"
