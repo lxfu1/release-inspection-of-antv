7b8dcf34154cfa98e12da59754a53407a2163731
liufu.lf <liufu.lf@antfin.com>
2021-05-21 10:43:06 +0800

    fix: test file

--- client/scripts/copy.js
+++ client/scripts/copy.js
@@ -4,4 +4,8 @@ shell.exec('cp -r ../../G2Plot/dist/g2plot.min.js ../server/static');
 // 删除 server 静态文件
 shell.exec('rm -rf ../server/static/assets');
 // 复制当前打包文件
-shell.exec('cp -r ./static/ ../server/static');
+shell.exec('cp -r ./static/* ../server/static/');
+
+shell.exec('ls ../server/static/');
+
+shell.exec('ls ../server/static/static/');
