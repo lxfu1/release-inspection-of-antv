0c4e71d22025088b058e7ca67afd14d9595ce647
liufu.lf <liufu.lf@antfin.com>
2021-05-21 10:08:01 +0800

    feat: rename static to assets

--- client/scripts/copy.js
+++ client/scripts/copy.js
@@ -2,6 +2,6 @@ const shell = require('shelljs');
 // 复制本地 g2plot.min.js
 shell.exec('cp -r ../../G2Plot/dist/g2plot.min.js ../server/static');
 // 删除 server 静态文件
-shell.exec('rm -rf ../server/static/static');
+shell.exec('rm -rf ../server/static/assets');
 // 复制当前打包文件
 shell.exec('cp -r ./static/ ../server/static');
